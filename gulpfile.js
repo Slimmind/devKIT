4c5fbc03d18a728d7e7703f8589ab14619469eb8
slimmind <paul.s@bigdropinc.com>
2016-09-13 18:03:52 +0300

    added pictures, changed gulp (but it still not compile right)

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,139 @@
 'use strict';
 
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    prefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rigger = require('gulp-rigger'),
-    cssClean = require('gulp-clean-css'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    rimraf = require('rimraf');
+var gulp = require('gulp');
+var newer = require('gulp-newer');
+var watch = require('gulp-watch');
+var prefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cmq = require('gulp-combine-media-queries');
+var csscomb = require('gulp-csscomb');
+var rigger = require('gulp-rigger');
+var cssClean = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+var rimraf = require('rimraf');
 
 var path = {
-    build: {
-        html: 'build/',
-        js: 'build/js/',
-        css: 'build/css/',
-        img: 'build/img/',
-        fonts: 'build/fonts/'
-    },
-    src: {
-        html: 'src/*.html',
-        js: 'src/js/main.js',
-        style: 'src/css/main.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
-    },
-    watch: {
-        html: 'src/**/*.html',
-        js: 'src/js/**/*.js',
-        style: 'src/css/**/*.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
-    },
-    clean: './build'
+  build: {
+    html: 'build/',
+    js: 'build/js/',
+    css: 'build/css/',
+    img: 'build/img/',
+    fonts: 'build/fonts/'
+  },
+  src: {
+    html: 'src/*.html',
+    js: 'src/js/**/*.js',
+    style: ['src/css/global.scss', 'src/css/pages/*.scss'],
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  },
+  watch: {
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    style: ['src/css/global.scss', 'src/css/pages/*.scss'],
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  },
+  clean: './build'
 };
 
 var config = {
-    server: {
-        baseDir: "./build"
-    },
-    tunnel: true,
-    host: 'localhost',
-    port: 9000,
-    logPrefix: "slimKit"
+  server: {
+    baseDir: "./build"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: "slimKit"
 };
 
 gulp.task('html:build', function () {
-    gulp.src(path.src.html)
-        .pipe(rigger())
-        .pipe(gulp.dest(path.build.html))
+  gulp.src(path.src.html)
+    .pipe(rigger())
+    .pipe(gulp.dest(path.build.html))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('js:build', function () {
-    gulp.src(path.src.js)
-        .pipe(rigger())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.js))
+  gulp.src(path.src.js)
+    .pipe(newer(path.build.js))
+    .pipe(rigger())
+    .pipe(uglify())
+    .pipe(gulp.dest(path.build.js))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('css:build', function () {
-    gulp.src(path.src.style)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(prefixer())
-        .pipe(cssClean())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css))
+  console.warn(path.src.style);
+  gulp.src(path.src.style)
+    .pipe(newer(path.build.css))
+    .pipe(sass())
+    .pipe(prefixer())
+    //.pipe(cssClean())
+    .pipe(csscomb())
+    .pipe(cmq({
+      log: true
+    }))
+    .pipe(gulp.dest(path.build.css))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('image:build', function () {
-    gulp.src(path.src.img)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(gulp.dest(path.build.img))
+  gulp.src(path.src.img)
+    .pipe(newer(path.build.img))
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(gulp.dest(path.build.img))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+gulp.task('fonts:build', function () {
+  gulp.src(path.src.fonts)
+    .pipe(newer(path.build.fonts))
+    .pipe(gulp.dest(path.build.fonts))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('build', [
-    'html:build',
-    'js:build',
-    'css:build',
-    'fonts:build',
-    'image:build'
+  'html:build',
+  'js:build',
+  'css:build',
+  'fonts:build',
+  'image:build'
 ]);
 
-gulp.task('watch', function(){
-    watch([path.watch.html], function(event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.style], function(event, cb) {
-        gulp.start('css:build');
-    });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
-    });
-    watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
-    });
+gulp.task('watch', function () {
+  watch([path.watch.html], function (event, cb) {
+    gulp.start('html:build');
+  });
+  watch([path.watch.js], function (event, cb) {
+    gulp.start('css:build');
+  });
+  watch([path.watch.js], function (event, cb) {
+    gulp.start('css:build');
+  });
+  watch([path.watch.img], function (event, cb) {
+    gulp.start('image:build');
+  });
+  watch([path.watch.fonts], function (event, cb) {
+    gulp.start('fonts:build');
+  });
+});
+
+gulp.task('serve', function () {
+  browserSync(config);
 });
 
 gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
+  rimraf(path.clean, cb);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'serve', 'watch']);
